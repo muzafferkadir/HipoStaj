944b70a37d2d67ef3bd964764d53c82788a67b4c
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2019-05-11 03:32:53 +0300

    Login page and views are created

--- HipoFood/recipe/urls.py
+++ HipoFood/recipe/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.ListingRecipes, name='Recipes'),
--- HipoFood/recipe/views.py
+++ HipoFood/recipe/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.utils import timezone
 from .models import Recipe
 from .forms import RecipeForm
-from django.contrib.auth.forms import *
-from django.contrib.auth import *
-from django.contrib.auth.decorators import *
-from django.http import *
-from django.shortcuts import redirect
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
 
 class RecipeListView(ListView):
 
@@ -41,20 +38,26 @@ def NewRecipe(request):
 
 
 def LoginPage(request):
-    form = AuthenticationForm
-    if(request.method=='POST'):
-        username = request.method['username']
-        password = request.method['password']
-        giris_kontrol = AuthenticationForm(data=request.POST)
-        if(giris_kontrol.is_valid()):
-            kullanici = authenticate(username=username,password=password)
-            login(request,kullanici)
-            return HttpResponseRedirect('/')
-    return render(request,'login.html',locals())
- 
-#def bilgi(request):
-#    oku = request.user.id
-#    if(not oku):
-#        return HttpResponseRedirect('/')
-#    return render(request,'bilgi.html',locals())
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "login.html",
+                    context={"form":form})
+    #username = request.POST.get('username')
+    #password = request.POST.get('password')
+    #user = authenticate(request,username=username,password=password)
+    #if user is not None:
+    #    login(request,user)
+    #    return render(request,'login.html',locals())
+    #else:
+    #    print("HATALI GİRİŞ")
+    #    return render(request,'login.html',locals())
 
