ae818176810eb3a15ac225fa2e57ba9d23848a9d
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2019-05-12 04:35:28 +0300

    Crispy forms are included and base.html is created for template

--- HipoFood/HipoFood/settings.py
+++ HipoFood/HipoFood/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'recipe',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
